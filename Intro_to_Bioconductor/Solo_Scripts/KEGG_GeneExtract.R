98d7e6e761665da243cdc72799fc2ead70246119
Abish Pius <60949830+abishpius@users.noreply.github.com>
2020-06-11 13:36:10 -0400

    Fixed to Write to Table

--- Intro_to_Bioconductor/Solo_Scripts/KEGG_GeneExtract.R
+++ Intro_to_Bioconductor/Solo_Scripts/KEGG_GeneExtract.R
@@ -1,3 +1,4 @@
+library(tidyverse)
 library(KEGGREST) #Install via Bioconductor if not done so already
 
 Kegg_genes <- keggGet("mmu04810")[[1]]$GENE 
@@ -7,11 +8,27 @@ Kegg_genes <- Kegg_genes[seq(2,length(Kegg_genes),2)]
 Kegg_genes <- gsub("\\;.*","", Kegg_genes)
 
 #Read in Gene Files, note txt files maybe different
-old <- read.csv("old.txt", header = FALSE)
-oldest <- read.csv("oldest.txt", header = FALSE)
+old <- read.csv("old.txt", header = FALSE)$v1
+oldest <- read.csv("oldest.txt", header = FALSE)$v1
 
 #Overlap with Old
-intersect(Kegg_genes,old)
+old_intersect<- intersect(Kegg_genes,old)
 
 #Overlap with oldest
-intersect(Kegg_genes, oldest)
+oldest_intersect<- intersect(Kegg_genes, oldest)
+
+#Read in Log2Files for Differential express Genes
+oldg <- read.csv("Oldk.csv")
+gerg <- read.csv("Ger.csv")
+
+#Extract Genes that Overlap
+Old_overlap <- oldg %>% 
+  filter(mgi_symbol %in% old_intersect) %>% select(log2FoldChange,mgi_symbol)
+Ger_overlap<- gerg %>% 
+  filter(mgi_symbol %in% oldest_intersect) %>% select(log2FoldChange,mgi_symbol)
+
+#Write out CSV files
+write.table(cbind(Old_overlap$mgi_symbol,Old_overlap$log2FoldChange),
+            file="delold.txt", sep="\t", row.names=FALSE, col.names=FALSE)
+write.table(cbind(Ger_overlap$mgi_symbol,Ger_overlap$log2FoldChange),
+            file="delger.txt", sep="\t", row.names=FALSE, col.names=FALSE)
